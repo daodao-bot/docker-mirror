e7df1c79b55d4ae37d1e109c3390f6724102ba43
DaoDao <625117069@qq.com>
2025-01-08 10:02:16 +0800

    😊

--- run.sh
+++ run.sh
@@ -9,16 +9,17 @@ echo "reading images..."
 
 while read -r line
 do
-    image="$line"
+    source_image="$line"
+    target_image=$(echo $source_image | sed 's/\//./g')
 
-    echo "pull image - $image"
-    docker pull $image
+    echo "pull image - $source_image"
+    docker pull $source_image
 
-    echo "tag image - $image"
-    docker tag $image $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_NAMESPACE/$image
+    echo "tag image - $source_image > $target_image"
+    docker tag $source_image $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_NAMESPACE/$target_image
 
-    echo "push image - $image"
-    docker push $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_NAMESPACE/$image
+    echo "push image - $target_image"
+    docker push $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_NAMESPACE/$target_image
 
 done < "$filename"
 

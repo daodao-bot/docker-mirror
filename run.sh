72a5e5ce093e73e49baff444f1afd4c9c7b19acc
DaoDao <625117069@qq.com>
2026-08-31 16:21:43 +0800

    😊

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ do
 #    docker push $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_NAMESPACE/$target_image
 
      echo "docker buildx"
-     docker buildx imagetools create --tag $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_NAMESPACE/$target_image $source_image
+     docker buildx imagetools create --tag $DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_NAMESPACE/$target_image $source_image --provenance=false
 
 done < "$filename"
 
